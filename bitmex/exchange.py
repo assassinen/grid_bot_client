231e035e47cb1be45a21d8109d778d4d00c69781
Ivan Novikov <i.novikov@iconic.vc>
2019-10-31 10:29:15 +0300

    fix sell-mode for one instrument

---
+++ bitmex/exchange.py
@@ -0,0 +1,58 @@
+from bitmex.session import Session
+
+class BitmexExchangeInterface:
+
+    def __init__(self, key, secret, base_url, api_url, instrument):
+        self.instrument = instrument
+        self.session = Session(key, secret, base_url, api_url)
+
+    def get_balance(self):
+        result = self.session.get('user/wallet')
+        return '{:.4f}'.format(result['amount'] / 10**8) if result else None
+
+    def get_positions(self):
+        query = '?filter=%7B%22symbol%22%3A%20%22{}%22%7D' \
+                '&columns=%5B%22avgEntryPrice%22%5D'.format(self.instrument)
+        result = self.session.get('position', query)
+        positions = result[0] if result else {}
+        return {'average_price': positions.get('avgEntryPrice'),
+                'size': positions.get('currentQty', 0)}
+
+    def get_last_trade_price(self):
+        query = '?symbol={}&count=1&reverse=true'.format(self.instrument)
+        result = self.session.get('trade', query)
+        trade_price = result[0] if result else {}
+        return trade_price['price'] if result else None
+
+    def get_last_order_price(self, side):
+        last_order_price = [order['price'] for order
+                            in self.get_open_orders()
+                            if order['side'] == side]
+        return last_order_price[0] if len(last_order_price) > 0 \
+            else self.get_last_trade_price()
+
+    def get_open_orders(self):
+        query = '?filter=%7B%22ordStatus%22%3A%20%22New%22%7D&reverse=true' \
+                '&columns=price%2C%20orderQty%2C%20side' \
+                '&symbol={}'.format(self.instrument)
+
+        open_orders = self.session.get('order', query)
+        return [{'price': order['price'],
+                 'orderQty': order['orderQty'],
+                 'side': order['side'].lower()}
+                for order in open_orders]
+
+    def create_order(self, order=''):
+        postdict = {
+            'symbol': self.instrument,
+            'side': order['side'].title(),
+            'orderQty': order['orderQty'],
+            'price': order['price'],
+            'ordType': 'Limit',
+            'execInst': 'ParticipateDoNotInitiate'
+        }
+        return self.session.post('order', postdict)
+
+    def cancel_all_orders(self):
+        postdict = {'symbol': self.instrument}
+        return self.session.delete('order/all', postdict)
---
+++ bitmex/session.py
@@ -0,0 +1,57 @@
+import requests
+import time
+import json
+import hmac
+import hashlib
+from models.log import setup_custom_logger
+
+class Session():
+
+    def __init__(self, key, secret, base_url, api_url):
+        self.key = key
+        self.secret = secret
+        self.base_url = base_url
+        self.api_url = api_url
+        self.logger = setup_custom_logger(f'session.{self.key}')
+
+    def generate_signature(self, expires, metod, path, data=None):
+        """Generate a request signature compatible with BitMEX."""
+        data = json.dumps(data) if data else ''
+        message = metod + path + str(expires) + data
+        signature = hmac.new(bytes(self.secret, 'utf8'), bytes(message, 'utf8'), digestmod=hashlib.sha256).hexdigest()
+        return signature
+
+    def get_headers(self, metod, path, data=''):
+        expires = int(time.time()) + 5
+        headers = {}
+        headers['api-expires'] = str(expires)
+        headers['api-key'] = self.key
+        headers['api-signature'] = self.generate_signature(expires, metod, path, data)
+        return headers
+
+    def request(self, metod, action, query='', data=''):
+        path = self.api_url + action + query
+        url = self.base_url + path
+        headers = self.get_headers(metod, path, data)
+        try:
+            if metod == 'GET':
+                response = requests.get(url=url, headers=headers)
+            if metod == 'POST':
+                response = requests.post(url=url, headers=headers, json=data)
+            if metod == 'DELETE':
+                response = requests.delete(url=url, headers=headers, json=data)
+        except Exception as r:
+            self.logger.info(r)
+
+        if response.status_code != 200:
+            raise Exception("Wrong response code: {0}".format(response.status_code))
+        return response.json()
+
+    def get(self, action, query=''):
+        return self.request('GET', action, query)
+
+    def post(self, action, postdict):
+        return self.request('POST', action, data=postdict)
+
+    def delete(self, action, postdict):
+        return self.request('DELETE', action, data=postdict)
