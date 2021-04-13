1f8fd13fd4e658d26bd173883b834f9d1d1f1fba
inovikov <i.novikov@city-mobil.ru>
2021-04-13 20:56:32 +0300

    поменял местамы запросы к брокеру

--- managers/orders_manager.py
+++ managers/orders_manager.py
@@ -35,10 +35,10 @@ class OrdersManager:
             return settings[0]
 
     def get_data_for_calculations(self, orders_state):
-        return {'last_prices': {'trade_price': self.exchange.get_last_trade_price(),
+        return {'active_orders': self.exchange.get_orders_state(orders_state),
+                'last_prices': {'trade_price': self.exchange.get_last_trade_price(),
                                 'order_price': self.exchange.get_last_order_price(self.settings.GRID_SIDE)},
-                'positions': self.exchange.get_positions(),
-                'active_orders': self.exchange.get_orders_state(orders_state)}
+                'positions': self.exchange.get_positions()}
 
     def replace_orders(self, to_create, to_cancel):
         orders_status = []
