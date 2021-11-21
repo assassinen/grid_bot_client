4343e23674687461363dd644d2f7cf88b61e0ef3
nii <assassinen@ya.ru>
2021-11-21 12:45:20 +0300

    фласк

---
+++ app.py
@@ -0,0 +1,5 @@
+from project import app
+
+if __name__ == '__main__':
+    app.run()
+    # app.run(host='127.0.0.2', port='1111')
---
+++ project/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+################
+#### config ####
+################
+
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+# app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
+from project.users.views import users_blueprint
+from project.home.views import home_blueprint
+
+# register our blueprints
+app.register_blueprint(users_blueprint)
+app.register_blueprint(home_blueprint)
+
+
+from .models import User
+
+login_manager.login_view = "users.login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter(User.id == int(user_id)).first()
