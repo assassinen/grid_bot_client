474a1453b60d84783520f32444095d78919b8aba
inovikov <i.novikov@city-mobil.ru>
2022-01-04 21:20:52 +0300

    Мега-Учебник Flask, Часть 7

--- project/wsgi.py
+++ project/wsgi.py
@@ -1,8 +1,19 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
-from project import app
+from project import app, db
 
 main = Blueprint('main', __name__)
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
 
 
 @main.route('/')
@@ -21,11 +32,6 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-
-@main.route('/profile')
-@login_required
-def profile():
-    return render_template('profile.html', name=current_user.username)
 
 if __name__ == '__main__':
     app.run()
