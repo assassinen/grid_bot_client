96294c5e506f02ffaf51a98241545b3230d7cb7d
nii <assassinen@ya.ru>
2022-01-05 01:45:06 +0300

    Мега-Учебник Flask, Глава 8

---
+++ project/create_posts.py
@@ -0,0 +1,40 @@
+from project.models import Post, User
+from project import app, db
+from datetime import datetime, timedelta
+
+u1 = User.query.filter_by(username='john').first()
+u2 = User.query.filter_by(username='susan').first()
+u3 = User.query.filter_by(username='mary').first()
+u4 = User.query.filter_by(username='david').first()
+
+
+# db.session.add_all([u3, u4])
+#
+# now = datetime.utcnow()
+# p1 = Post(body="post from john", author=u1,
+#           timestamp=now + timedelta(seconds=1))
+# p2 = Post(body="post from susan", author=u2,
+#           timestamp=now + timedelta(seconds=4))
+# p3 = Post(body="post from mary", author=u3,
+#           timestamp=now + timedelta(seconds=3))
+# p4 = Post(body="post from david", author=u4,
+#           timestamp=now + timedelta(seconds=2))
+# db.session.add_all([p1, p2, p3, p4])
+# db.session.commit()
+
+# setup the followers
+# u1.follow(u2)  # john follows susan
+# u1.follow(u4)  # john follows david
+# u2.follow(u3)  # susan follows mary
+# u3.follow(u4)  # mary follows david
+# db.session.commit()
+
+# check the followed posts of each user
+f1 = u1.followed_posts().all()
+f2 = u2.followed_posts().all()
+f3 = u3.followed_posts().all()
+f4 = u4.followed_posts().all()
+print(u1, f1)
+print(u2, f2)
+print(u3, f3)
+print(u4, f4)
--- project/routes/users.py
+++ project/routes/users.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_user, current_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from .. import db
-from ..forms import LoginForm, RegistrationForm, EditProfileForm
+from ..forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
 from ..models import User
 
 users = Blueprint('users', __name__)
@@ -63,7 +63,8 @@ def profile(username):
         {'author': user, 'body': 'Test post #1'},
         {'author': user, 'body': 'Test post #2'}
     ]
-    return render_template('profile.html', user=user, posts=posts)
+    form = EmptyForm()
+    return render_template('profile.html', user=user, posts=posts, form=form)
 
 
 @users.route('/edit_profile', methods=['GET', 'POST'])
@@ -82,3 +83,41 @@ def edit_profile():
     return render_template('edit_profile.html', title='Edit Profile', form=form)
 
 
+@users.route('/follow/<username>', methods=['POST'])
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot follow yourself!')
+            return redirect(url_for('users.profile', username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flash('You are following {}!'.format(username))
+        return redirect(url_for('users.profile', username=username))
+    else:
+        return redirect(url_for('index'))
+
+
+@users.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot unfollow yourself!')
+            return redirect(url_for('users.profile', username=username))
+        current_user.unfollow(user)
+        db.session.commit()
+        flash('You are not following {}.'.format(username))
+        return redirect(url_for('users.profile', username=username))
+    else:
+        return redirect(url_for('index'))
