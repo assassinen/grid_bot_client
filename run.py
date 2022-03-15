010e5403711acb20bb29029de79c82477de494c7
inovikov <i.novikov@city-mobil.ru>
2022-03-15 12:35:38 +0300

    example

--- run.py
+++ run.py
@@ -3,12 +3,14 @@ from managers.orders_manager import OrdersManager
 from managers.bots_manager import BotsManager
 import asyncio
 
-bots = BotsManager()
-# orders_сalculators = bots.get_orders_сalculators()
+# bots = BotsManager()
+# # orders_сalculators = bots.get_orders_сalculators()
+#
+# loop = asyncio.get_event_loop()
+#
+# for orders_manager in bots.get_orders_managers():
+#     loop.create_task(orders_manager.run_loop())
+# loop.run_forever()
+#
 
-loop = asyncio.get_event_loop()
-
-for orders_manager in bots.get_orders_managers():
-    loop.create_task(orders_manager.run_loop())
-loop.run_forever()
-
+print("start")
