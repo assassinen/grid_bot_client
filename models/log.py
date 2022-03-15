c3472f29afae0d5be645ee19061f5d74e1fd9b91
inovikov <i.novikov@city-mobil.ru>
2022-03-15 18:43:38 +0300

    logs

--- models/log.py
+++ models/log.py
@@ -2,7 +2,8 @@ import logging
 
 def setup_custom_logger(name):
     logger = logging.getLogger(name)
-    log_format = '%(asctime)s - %(name)s - %(message)s'
+    # log_format = '%(asctime)s - %(name)s - %(message)s'
+    log_format = '%(name)s - %(message)s'
     logging.basicConfig(format=log_format, level=logging.INFO)
 
     return logger
